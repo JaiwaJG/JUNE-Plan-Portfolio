6645b6750300d709490d44cd64ef0a2b5852f178
Jaiwa Team <jaiwagum@gmail.com>
2026-06-09 19:15:48 +0630

    Add responsive navigation menu and update styles for better usability

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function showMessage() {
     window.open("https://t.me/JaiwaJG","_blank");
 }
+const menuBtn = document.querySelector('.menu-btn');
+const navRight = document.querySelector('.nav-right');
+menuBtn.addEventListener('click', () => {
+    navRight.classList.toggle('active');
+}
+);
