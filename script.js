c798556b2d2a836e7282e92294d765279e7f3e39
Jaiwa Team <jaiwagum@gmail.com>
2026-06-13 03:47:10 +0630

    v001

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   navRight.classList.toggle("active");
 });
+
+const navLinks = document.querySelectorAll(".nav-right a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        menuBtn.classList.remove("active");
+        navRight.classList.remove("active");
+    });
+});
